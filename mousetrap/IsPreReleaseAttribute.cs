571f4f12a23130c4a63da2b7bba304b4db5c48e5
radj307 <radj307@gmail.com>
2022-02-26 02:55:27 -0500

    add needed hooks for SetVersion.ps1

---
+++ mousetrap/IsPreReleaseAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace VolumeControl
+{
+    [System.AttributeUsage(System.AttributeTargets.Assembly, Inherited = false, AllowMultiple = false)]
+    internal sealed class IsPreReleaseAttribute : System.Attribute
+    {
+        public string IsPreRelease { get; }
+        public IsPreReleaseAttribute(string isPreRelease) => IsPreRelease = isPreRelease;
+    }
+}
